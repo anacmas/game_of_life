d75af9ca81d0f4385950d7f40015580197e09141
anasilva <anacmas@gmail.com>
2023-01-09 16:10:01 -0300

    feat: color in the grid with the logics working and buttons to play

--- life_vue/src/components/GameOfLife.vue
+++ life_vue/src/components/GameOfLife.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="hello">
-    <table>
-      <tr v-for="(line, index) in grid" :key="grid[index]">
-        <td
-          v-for="(cell, idx) in line"
-          :key="idx"
-          @click="handleClick(index, idx)"
-          :class="cell == 1 ? 'active' : 'non-active'"
-        ></td>
-      </tr>
-    </table>
+  <div>
+    <div class="board">
+      <table>
+        <tr v-for="(line, index) in grid" :key="index">
+          <td
+            v-for="(cell, idx) in line"
+            :key="idx"
+            @click="handleClick(index, idx)"
+            :class="cell == 1 ? 'active' : 'non-active'"
+          ></td>
+        </tr>
+      </table>
+    </div>
+    <div class="buttons">
+      <button class="start">Start</button>
+      <button class="stop">Stop</button>
+      <button class="clear">Clear</button>
+    </div>
   </div>
 </template>
 
@@ -250,7 +257,7 @@ export default {
   methods: {
     handleClick(index, idx) {
       this.active = !this.active;
-      this.grid[index][idx] = !this.grid[index][idx];
+      this.grid[index].splice(idx, 1, !this.grid[index][idx]);
       console.log(this.grid);
     },
   },
@@ -264,16 +271,37 @@ export default {
 }
 
 td {
-  border: 1px solid black;
-  width: 15px;
-  height: 15px;
+  width: 20px;
+  height: 20px;
 }
 
 .active {
-  background-color: rgb(9, 210, 66);
+  background-color: rgb(237, 108, 172);
 }
 
 .non-active {
-  background-color: rgb(255, 47, 0);
+  background-color: rgb(188, 177, 219);
+}
+
+button {
+  width: 60px;
+  height: 30px;
+  margin-left: 10px;
+  border: none;
+  cursor: pointer;
+  border-radius: 3px;
+}
+
+button:hover {
+  opacity: 60%;
+  transition: 0.3s;
+}
+
+.start {
+  background-color: rgb(206, 245, 245);
+}
+
+.stop {
+  background-color: rgb(255, 215, 215);
 }
 </style>
